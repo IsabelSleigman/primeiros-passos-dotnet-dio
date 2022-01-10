ab5824a99e20cb8e9e0c4290dab1293e5ba269a2
IsabelSleigman <sleimusic@gmail.com>
2022-01-09 21:14:31 -0300

    criado gitignore e testado proram

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int numeroDeVezes = 5;
+
+for (int i = 0; i < numeroDeVezes; i++)
+{
+    Console.WriteLine($"Bem-vindo ao curso de .Net {i}");
+};
