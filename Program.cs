a20675635de69fb98df63b2219687a8c607aa042
isabelsleigman <sleimusic@gmail.com>
2022-01-12 11:58:27 -0300

    Criado menu interação

--- Program.cs
+++ Program.cs
@@ -1,6 +1,39 @@
-﻿int numeroDeVezes = 5;
+﻿
+    Console.WriteLine("Informe a opção desejada: ");
+    Console.WriteLine("1 - Inserir novo aluno");
+    Console.WriteLine("2 - Listar alunos");
+    Console.WriteLine("3 - Calcular média geral");
+    Console.WriteLine("X - Sair");
+    Console.WriteLine();
 
-for (int i = 0; i < numeroDeVezes; i++)
-{
-    Console.WriteLine($"Bem-vindo ao curso de .Net {i}");
-};
+    string opUsuario = Console.ReadLine();
+
+    while (opUsuario.toUpper() != "X")
+    {
+        switch(opUsuario)
+        {
+            case "1":
+                // Adicionar aluno
+                break;
+
+            case "2":
+                // Listar alunos
+                break;
+
+            case "3":
+                //Calcular media geral
+                break;
+            
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+
+        Console.WriteLine("Informe a opção desejada: ");
+        Console.WriteLine("1 - Inserir novo aluno");
+        Console.WriteLine("2 - Listar alunos");
+        Console.WriteLine("3 - Calcular média geral");
+        Console.WriteLine("X - Sair");
+        Console.WriteLine();
+
+        opUsuario = Console.ReadLine();
+    }
